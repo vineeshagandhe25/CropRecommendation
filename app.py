f72cbe3f828779fb69f3549c423551a0315fb20b
vineeshagandhe25 <vineeshagandhe25@gmail.com>
2024-10-26 12:09:55 +0530

    modified code in app.py

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def predict():
     return render_template('result.html', recommended_crop=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
